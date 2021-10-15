6c7dace97312297236586fa9f5710db7d6212f1b
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2021-10-15 21:19:32 +0900

    eloquent

---
+++ app/Models/Person.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    public function getData()
+    {
+        return $this->id . '： ' . $this->name . ' (' . $this->age . ')';
+    }
+}
