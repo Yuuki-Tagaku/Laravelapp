ce5ab0b99efd9b90862a57f25f286028c33e937d
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2021-10-21 23:03:31 +0900

    belongsTo and others

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -18,7 +18,7 @@ class Board extends Model
 
     public function getData()
     {
-        return $this->id . ': ' . $this->title . ' (' . $this->person->name . ')';
+        return $this->id . ': ' . $this->title . ' (' . $this->person['name'] . ')';
     }
 
     public function person()
